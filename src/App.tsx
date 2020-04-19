3edbac9985dc4b176c834dccf73882678245b685
Masashi <ume.app555@gmail.com>
2020-04-19 22:02:45 +0900

    react on typescript

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+
+class App extends Component {
+  handleClick = () => {
+    console.log('hoge');
+  };
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <h1> TypeScript </h1>
+        <button type="button" onClick={this.handleClick}>
+          click me
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const isDebug = process.env.NODE_ENV !== 'production';
+console.log(`debug is ${isDebug}`);
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: isDebug ? 'development' : 'production',
+  devtool: isDebug ? 'source-map' : false,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: isDebug
+                  ? '[path][name]__[local]'
+                  : '[hash:base64:8]',
+              },
+              sourceMap: isDebug,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('cssnano')({
+                  zindex: false,
+                }),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hot: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
